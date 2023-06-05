b7a07525d2ac0445fdec59b6cecc21505a663eab
Hein Htet Zaw <bogeyhz.325@gmail.com>
2023-06-05 21:39:57 +0630

    afdadsfsdfaf

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -5,36 +5,13 @@ import React from "react";
 
 const Navbar = () => {
 
-//   const token = Cookies.get("token");
-//   const auth = token ? true : false;
- 
 
-;
-
-
- 
-
-  // const user = JSON.parse(Cookies.get('user'))
-
-  // console.log(user);
   return (
     <>
       <div className=" flex py-3 bg-gray-300 lg:px-10 px-5 justify-between items-center sticky top-0">
         <h1 className=" text-2xl lg:text-3xl font-semibold select-none cursor-pointer">
           Contactify
         </h1>
-        {/* simple button before authentication */}
-        {/* <div className={`flex gap-3  lg:gap-5 ${auth ? "hidden" : "flex"}`}>
-          <a href={"/login"}>
-            <button className="  outline outline-offset-0 outline-gray-300 py-2  px-3 rounded text-color hover:bg-orange-500 hover:text-white duration-300 lg:hover:text-gray-800">
-              Login
-            </button>
-          </a>
-         
-       
-        </div> */}
-
-        {/* After authentication */}
       
 
       </div>
--- src/components/sidebar/Sidebar.jsx
+++ src/components/sidebar/Sidebar.jsx
@@ -12,76 +12,7 @@ const Sidebar = () => {
       <div className=" w-fit h-[80vh] lg:h-[85vh] flex flex-col py-5 gap-5 lg:gap-10 fixed ">
         <div className=" w-full px-0 lg:px-2 flex">
             side bar
-          {/* <div className=" flex items-center gap-3 shadow-xl bg-orange-100 text-xl py-2 px-5 justify-center rounded-full" onClick={() => {
-             nav('/contacts/create')
-          }}>
-            <AiOutlinePlus size={"1.5rem"} />
-            <button className="">Create contact</button>
-          </div> */}
-        </div>
-        {/* <div className=" w-full  lg:px-0">
-          <a
-            href={"/"}
-            className="flex items-center justify-between rounded-full text-md py-2 lg:rounded-none lg:rounded-e-full px-5 menu_hover"
-          >
-            <div className="flex gap-5 items-center">
-              <AiFillHome />
-              <button>Home</button>
-            </div>
-          </a>
-          <a
-            href={"/dashboard"}
-            className={
-              "flex items-center justify-between rounded-full text-md py-2 lg:rounded-none lg:rounded-e-full px-5 menu_hover"
-            }
-          >
-            <div className="flex gap-5 items-center">
-              <BsFillPersonFill />
-              <button>Contacts</button>
-            </div>
-            <Badge color="yellow">{contactsData?.length}</Badge>
-          </a>
-          <a
-            href={"/recently_search"}
-            className="flex items-center justify-between rounded-full text-md py-2 lg:rounded-none lg:rounded-e-full px-5 menu_hover"
-          >
-            <div className="flex gap-5 items-center">
-              <MdRecentActors />
-              <button>Recently visit</button>
-            </div>
-          </a>
-          <a
-            href={"/favourite"}
-            className="flex items-center justify-between rounded-full text-md py-2 lg:rounded-none lg:rounded-e-full px-5 menu_hover"
-          >
-            <div className="flex gap-5 items-center">
-              <MdOutlineFavorite />
-              <button>Favourite</button>
-            </div>
-            <Badge color="yellow">{favorite?.length}</Badge>
-          </a>
-        </div>
-        <div className=" w-full"> */}
-          <h1 className=" text-xl px-5 py-2 font-semibold">Fix and manage</h1>
-          <a
-            href={"/Merge_fix"}
-            className="flex items-center justify-between rounded-full text-md py-2 lg:rounded-none lg:rounded-e-full px-5 menu_hover"
-          >
-            <div className="flex gap-5 items-center">
-              <MdOutlineAutoFixHigh />
-              <button>Merge and fix</button>
-            </div>
-          </a>
-          <a
-            href={"/trash"}
-            className="flex items-center justify-between rounded-full text-md py-2 lg:rounded-none lg:rounded-e-full px-5 menu_hover"
-          >
-            <div className="flex gap-5 items-center">
-              <BsTrash />
-              <button>Bin</button>
-            </div>
-          </a>
-        {/* </div> */}
+       </div>
       </div>
     </>
   );
